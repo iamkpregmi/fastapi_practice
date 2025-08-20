00bf26fc6127a2deb185522be6ac064037be39b8
krishna <iamkpregmi@gmail.com>
2025-08-20 21:52:03 +0530

    update school magmt dashboard advance join and calculation

--- components/school_mgmt/crud.py
+++ components/school_mgmt/crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from models.models import School, Student, Library
 from components.test_component import schema
-from sqlalchemy import func
+from sqlalchemy import func, desc
 
 
 def get_student_details(school_id, db):
@@ -74,7 +74,10 @@ def get_school_list_by_id(request, db):
     return {'data': data_list}
 
 
+#Dashboard and Calculation part
 def school_dashboard(db):
+    #---------------------------------------------
+    #Group by course and gender
     studet_data = db.query(
         Student.course,
         Student.gender,
@@ -95,5 +98,32 @@ def school_dashboard(db):
         "avg_fee": stu.avg_fee
     } for stu in studet_data]
 
-    return {'data': data_list}
+
+    #------------------------------------------
+    #City by total fee using join
+    fee_data = db.query(
+        School.city,
+        func.sum(Student.fee).label('total_fee')
+    ).join(
+        Student,
+        School.id == Student.school_id
+    ).group_by(
+        School.city
+    ).order_by(
+        desc(func.sum(Student.fee))
+    ).all()
+
+    fee_data = [
+        {
+            'city': fee.city,
+            'total_fee': fee.total_fee
+        }
+        for fee in fee_data]
+    
+    context = {
+        'studet_data': data_list,
+        'fee_data': fee_data
+    }
+
+    return {'data': context}
 
